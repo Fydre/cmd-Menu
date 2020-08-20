113673f7a0bb68b953952358a98fa29b3474c944
Fydre <64918100+Fydre@users.noreply.github.com>
2020-08-20 09:45:48 +0200

    Add files via upload

--- Console.bat
+++ Console.bat
@@ -1,8 +1,5 @@
 @echo off
 cd functions
-for /f "Delims=" %%a in (passwords.sec) do (
-	set password=%%a
-)
 chcp 65001 >nul
 SETLOCAL ENABLEDELAYEDEXPANSION
 set "CCD=%CD%"
@@ -14,10 +11,6 @@ for /f "Delims=" %%a in (warn.bool) do (
 		echo true>warn.bool
 		exit
 	)
-)
-set sleepseconds=1
-for /f "Delims=" %%a in (loggedin.bool) do (
-	set loggedin=%%a
 )
 :ifretry
 echo The password system is broken.
@@ -31,7 +24,7 @@ if exist selector.char (
 ) else set cursor=■
 if exist colors.clr (
 	for /f "Delims=" %%d in (colors.clr) do (
-		color %%d >nul
+		color %%d
 		set dClr=%%d
 	)
 ) else (
@@ -72,16 +65,16 @@ set "l3= "
 set "l4= "
 set l%light%=%cursor%
 echo.
-echo [0m                                 Press ↑ or ↓ key to select.
+echo                                  Press ↑ or ↓ key to select.
 echo                                  Press ENTER key to enter.
 echo                                  Press ESC key to go back.
 echo.
-echo [1;34m            ┌────────────────────┐[0m
-echo [1;34m            │[1;36;43m░░░%l1%░░░░Apps░░░░%l1%░◘░[0m[1;34m│ ╔════╗[0m
-echo [1;34m            │[1;36;43m░░░%l2%░░Settings░░%l2%░☼░[0m[1;34m│ ║[1;32mMENU[1;34m║[0m
-echo [1;34m            │[1;36;43m░░░%l3%░░░Games░░░░%l3%░☻░[0m[1;34m│ ╚════╝[0m
-echo [1;34m            │[1;36;43m░░░%l4%░░░░Exit░░░░%l4%░►░[0m[1;34m│[0m
-echo [1;34m            └────────────────────┘[0m
+echo [1;34m            ┌────────────────────┐
+echo             │[1;36;43m░░░%l1%░░░░Apps░░░░%l1%░◘░[0m[1;34m│ ╔════╗
+echo             │[1;36;43m░░░%l2%░░Settings░░%l2%░☼░[0m[1;34m│ ║[1;32mMENU[1;34m║
+echo             │[1;36;43m░░░%l3%░░░Games░░░░%l3%░☻░[0m[1;34m│ ╚════╝
+echo             │[1;36;43m░░░%l4%░░░░Exit░░░░%l4%░◄░[0m[1;34m│
+echo             └────────────────────┘[0m
 echo.
 :input1
 keystick
@@ -96,7 +89,6 @@ if %key%==328 (
 	set %key%=0
 	goto logo
 ) else if %key%==13 (
-	sound %cd%\Enter.wav
 	if %light%==1 (
 		set %key%=0
 		goto apps
@@ -126,22 +118,24 @@ set light=1
 mode 61,16
 title Apps
 cls
-set l1=
-set l2=
-set l3=
-set l4=
+set "l1= "
+set "l2= "
+set "l3= "
+set "l4= "
+set "l5= "
 set l%light%=%cursor%
 echo.
 echo [0m                                 Press ↑ or ↓ key to select.
 echo                                  Press ENTER key to enter.
 echo                                  Press ESC key to go back.
 echo.
-echo [1;34m            ┌───────────────────────┐[0m
-echo [1;34m            │[1;36;43m▒▒%l1%▒Unicode Search▒%l1%▒⅍▒[0m[1;34m│ ╔════╗[0m
-echo [1;34m            │[1;36;43m▒▒%l2%▒▒▒▒Notepad▒▒▒▒▒%l2%▒A▒[0m[1;34m│ ║[1;32mAPPS[1;34m║[0m
-echo [1;34m            │[1;36;43m▒▒%l3%▒▒▒FunThings▒▒▒▒%l3%▒☺▒[0m[1;34m│ ╚════╝[0m
-echo [1;34m            │[1;36;43m▒▒%l4%▒▒▒▒Explorer▒▒▒▒%l4%▒¶▒[0m[1;34m│[0m
-echo [1;34m            └───────────────────────┘[0m
+echo [1;34m            ┌────────────────────────┐[0m
+echo [1;34m            │[1;36;43m▒▒%l1%▒Unicode Search▒▒%l1%▒⅍▒[0m[1;34m│ ╔════╗[0m
+echo [1;34m            │[1;36;43m▒▒%l2%▒▒▒▒▒Notepad▒▒▒▒▒%l2%▒A▒[0m[1;34m│ ║[1;32mAPPS[1;34m║[0m
+echo [1;34m            │[1;36;43m▒▒%l3%▒▒▒▒FunThings▒▒▒▒%l3%▒☺▒[0m[1;34m│ ╚════╝[0m
+echo [1;34m            │[1;36;43m▒▒%l4%▒▒▒▒Explorer▒▒▒▒▒%l4%▒¶▒[0m[1;34m│[0m
+echo [1;34m            │[1;36;43m▒▒%l5%▒Animation Maker▒%l5%▒►▒[0m[1;34m│[0m
+echo [1;34m            └────────────────────────┘[0m
 echo.
 :input2
 keystick
@@ -156,7 +150,6 @@ if %key%==328 (
 	set %key%=0
 	goto menu
 ) else if %key%==13 (
-	sound %cd%\Enter.wav
 	if %light%==1 (
 		set %key%=0
 		goto uni
@@ -173,19 +166,95 @@ if %key%==328 (
 		set %key%=0
 		goto explore
 	)
+	if %light%==5 (
+		set %key%=0
+		goto anim
+	)
 ) else (
 	set %key%=0
 	goto input2
 )
-if %light% gtr 4 set light=1
-if %light% lss 1 set light=4
+if %light% gtr 5 set light=1
+if %light% lss 1 set light=5
 goto appsmenu
+:anim
+title Animation Maker
+echo Work in Progress...
+pause >nul
+goto apps
+cd anim
+mode 40,14
+echo ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+echo ▓▓▓▓[1;34m1. New Animation       [0m▓▓▓▓
+echo ▓▓▓▓[1;32m2. Run Animation       [0m▓▓▓▓
+echo ▓▓▓▓[1;31m3. Delete Animation    [0m▓▓▓▓
+echo ▓▓▓▓[1;37m4. Exit Animation Maker[0m▓▓▓▓
+echo ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+choice /c 1234 >nul
+echo %Errorlevel%
+pause
+if %errorlevel%==1 (
+	goto newanim
+) else if %errorlevel%==2 (
+	goto oldanim
+) else if %errorlevel%==3 (
+	cls
+	echo.
+	set /p "delanim=[4;1;34mAnimation:[0m "
+) else (
+	goto apps
+)
+:oldanim
+echo.
+set /p "animname=[4;1;34mAnimation Name:[0m "
+echo.
+cd %animname%
+cls
+If exist frames.num (
+	for /f "Delims=" %%a in (frames.num) do set frames=%%a
+	for /l %%f in (1,1,%frames%) do (
+		type f%%f.frame
+		ping localhost -n 1 >nul
+		cls
+	)
+)
+goto anim
+:newanim
+echo.
+set /p "name=[4;1;34mName:[0m "
+md "%name%"
+cd "%name%"
+set /p "frames=[4;1;34mFrames:[0m "
+echo %frames%>frames.num
+cls
+echo ―†‡•…‴⁞←─│┌┐└┘├┤┬┴◌┼═║╧╨╩╪╫╬
+echo ▀▄█ ▌▐░▒▓■□
+echo.
+for /l %%f in (1,1,%frames%) do (
+	for /l %%l in (1,1,10) do set "line%%l= "
+	for /l %%d in (1,1,10) do set /p "line%%d= "
+	(
+		echo %line1%
+		echo %line2%
+		echo %line3%
+		echo %line4%
+		echo %line5%
+		echo %line6%
+		echo %line7%
+		echo %line8%
+		echo %line9%
+		echo %line10%
+	) >f%%f.frame
+	cls
+)
+cd ..\..
+goto apps
 :explore
 set "dirc=."
 mode 80,50
 title Explorer
 cls
-echo [0m
+echo.
 echo                   Type .. to go back.
 echo                   Type exit to exit.
 echo [1;33;43m▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
@@ -197,7 +266,7 @@ echo.
 echo [1;33;43m▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 echo ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓[0m[4;1;34m
 set /p dirc=Choose Directory:[0m
-if %dirc%==^exit (
+if "%dirc%"=="exit" (
 	cd %CCD%
 	goto apps
 )
@@ -261,16 +330,14 @@ echo ▓[43m ∞ ∟ ∩ ∫ ≈ ≡ ⌂ ⌐ ⌠ ⌡ ▀ ▄ █ ▌ ▐ ░ �
 echo ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 echo.
 :writeuni
-set unitext=.
-set /p unitext=
-if "%unitext%==^e^x^i^t" (
+set "unitext=n"
+set /p "unitext= "
+set delay=0
+if "%unitext%"=="exit" (
 	goto appsmenu
-) else if "%unitext%==clear" (
+) else if "%unitext%"=="clear" (
 	goto uni
-) else (
-	goto writeuni
-)
-pause
+) else goto writeuni
 goto apps
 :: Apps Section End
 :: Settings Beginning
@@ -291,12 +358,12 @@ echo                                  Press ↑ or ↓ key to select.
 echo                                  Press ENTER key to enter.
 echo                                  Press ESC key to go back.
 echo.
-echo [1;34m            ┌──────────────────┐[0m
-echo [1;34m            │[1;36;43m▒▒%l1%▒▒▒Color▒▒▒%l1%▒█▒[0m[1;34m│ ╔════╗[0m
-echo [1;34m            │[1;36;43m▒▒%l2%▒▒Cursor▒▒▒%l2%▒•▒[0m[1;34m│ ║[1;32mAPPS[1;34m║[0m
-echo [1;34m            │[1;36;43m▒▒%l3%▒▒▒Reset▒▒▒%l3%▒◌▒[0m[1;34m│ ╚════╝[0m
-echo [1;34m            │[1;36;43m▒▒%l4%▒Password▒▒%l4%▒^*▒[0m[1;34m│[0m
-echo [1;34m            └──────────────────┘[0m
+echo [1;34m            ┌──────────────────┐
+echo             │[1;36;43m▒▒%l1%▒▒▒Color▒▒▒%l1%▒█▒[0m[1;34m│ ╔════╗
+echo             │[1;36;43m▒▒%l2%▒▒Cursor▒▒▒%l2%▒•▒[0m[1;34m│ ║[1;32mAPPS[1;34m║
+echo             │[1;36;43m▒▒%l3%▒▒▒Reset▒▒▒%l3%▒◌▒[0m[1;34m│ ╚════╝
+echo             │[1;36;43m▒▒%l4%▒Password▒▒%l4%▒^*▒[0m[1;34m│
+echo             └──────────────────┘[0m
 echo.
 :input3
 keystick
@@ -311,7 +378,6 @@ if %key%==328 (
 	set %key%=0
 	goto menu
 ) else if %key%==13 (
-	sound %cd%\Enter.wav
 	if %light%==1 (
 		set %key%=0
 		goto color
@@ -347,7 +413,7 @@ echo          3 ^= greenblue     B ^= ^*      greenblue
 echo          4 ^= red           C ^= ^*      red
 echo          5 ^= purple        D ^= ^*      purple
 echo          6 ^= yellow        E ^= ^*      yellow
-echo          7 ^= bright gray   F ^= white           [0m
+echo          7 ^= bright gray   F ^= white[0m
 echo ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 set /p textclr=Text and foreground Color:
 set /p background=Background Color:
@@ -372,14 +438,15 @@ echo ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 echo ▓▓▓▓▓▓▓▓▓Yes  ^|    No▓▓▓▓▓▓▓▓▓▓
 set submit=noanswer
 set /p submit=▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓[0m
-if %submit%==yes (
-	echo configNoPasswordSet>passwords.sec
+if /i "%submit%"=="yes" (
+	del passwords.sec
 	del colors.clr
-	echo true>loggedin.bool
 	del selector.char
 	echo false>warn.bool
+	rd /s /q anim
+	md anim
 	exit
-) else if %submit%==no (
+) else if /i "%submit%"=="no" (
 	goto custom
 ) else (
 	goto submitrest
@@ -402,16 +469,18 @@ title Apps
 cls
 set "l1= "
 set "l2= "
+set "l3= "
 set l%light%=%cursor%
 echo.
 echo                                  Press ↑ or ↓ key to select.
 echo                                  Press ENTER key to enter.
 echo                                  Press ESC key to go back.
 echo.
-echo [1;34m            ┌─────────────────────┐[0m
-echo [1;34m            │[1;36;43m▒▒%l1%▒▒▒▒▒Ship▒▒▒▒▒%l1%▒▬▒[0m[1;34m│ ╔═════╗[0m
-echo [1;34m            │[1;36;43m▒▒%l2%▒Apple▒Basket▒%l2%▒●▒[0m[1;34m│ ║[1;32mGames[1;34m║[0m
-echo [1;34m            └─────────────────────┘ ╚═════╝[0m
+echo [1;34m            ┌─────────────────────┐
+echo             │[1;36;43m▒▒%l1%▒▒▒▒▒Ship▒▒▒▒▒%l1%▒▬▒[0m[1;34m│ ╔═════╗
+echo             │[1;36;43m▒▒%l2%▒Apple Basket▒%l2%▒●▒[0m[1;34m│ ║[1;32mGames[1;34m║
+echo             │[1;36;43m▒▒%l3%▒▒▒▒Racer▒▒▒▒▒%l3%▒●▒[0m[1;34m│ ╚═════╝
+echo             └─────────────────────┘[0m
 echo.
 :input4
 keystick
@@ -426,7 +495,6 @@ if %key%==328 (
 	set %key%=0
 	goto menu
 ) else if %key%==13 (
-	sound %cd%\Enter.wav
 	if %light%==1 (
 		set %key%=0
 		cls
@@ -437,10 +505,15 @@ if %key%==328 (
 		cls
 		call applegame
 	)
+	if %light%==3 (
+		set %key%=0
+		cls
+		call racer
+	)
 ) else (
 	set %key%=0
 	goto input4
 )
-if %light% gtr 2 set light=1
-if %light% lss 1 set light=2
+if %light% gtr 3 set light=1
+if %light% lss 1 set light=3
 goto gamemenu
